a102fd4c1d11b6ddd29e2f286ec34a33a09561bc
Nicolly Rocha <nicollyrcs@gmail.com>
2021-11-18 16:08:37 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./component/MovieRow";
+import "./App.css";
+
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+      console.log(list);
+    };
+    loadAll();
+  }, []);
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+};
